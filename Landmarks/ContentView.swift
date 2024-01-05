26a3c926273bfc52505c66b5775c31891bb72807
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2024-01-05 22:47:46 +0545

    added more content

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -30,7 +30,13 @@ struct ContentView: View {
                               Text("About Turtle Rock")
                                   .font(.title2)
                 
-                              Text("Descriptive text goes here.")
+                
+                              Text("""
+Turtle Rock is a rock formation in Joshua Tree National Park, California.
+    This massive formation is located across from the parking lot from the entrance into Real Hidden Valley area in Joshua Tree National Park. The northwest face of Turtle Rock is several hundred feet long and works like a massive wall protecting a pleasant picnic area with trees, benches, and even bathrooms. On any weekend during the climbing season, you may see several climbing classes in progress under the trees and around many boulders strewn on the base of the northwest face. You will also run into families picnicking and children scrambling on smaller low angle rocks.
+
+Turtle Rock, being the most prominent rock formation in this area, attracts many tour buses and tourists. The rock practically covers the entire southern parameter of the parking lot giving the area a sense of being enclosed and protected. You may find yourself being invited to special events held in the parking lot such as religious ceremonies or star gazing parties. Amateur and professional astronomers bring their telescopes and invite the public to observe deep sky objects such as galaxies and globular clusters.
+""")
             }.padding(.all)
             Spacer()
         }
