50ece1aeff3b85d09705d8e8add6aa4b4ac0fb73
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2024-01-06 23:34:08 +0545

    added images and a image field on Landmark

--- Landmarks/Landmark.swift
+++ Landmarks/Landmark.swift
@@ -5,7 +5,7 @@
 //  Created by Shishir Rijal on 06/01/2024.
 //
 
-import Foundation
+import SwiftUI
 
 struct Landmark: Hashable, Codable {
     var id: Int
@@ -13,4 +13,8 @@ struct Landmark: Hashable, Codable {
     var park: String
     var state: String
     var description: String
+    private var imageName: String
+    var image: Image{
+        Image(imageName)
+    }
 }
