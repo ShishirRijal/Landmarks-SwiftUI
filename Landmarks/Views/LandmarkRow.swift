26e8b27f0a9daf794a24409023daaf47dd2ac4f2
Shishir Rijal <63596895+ShishirRijal@users.noreply.github.com>
2024-01-07 20:22:55 +0545

    added landmark row

---
+++ Landmarks/Views/LandmarkRow.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkRow.swift
+//  Landmarks
+//
+//  Created by Shishir Rijal on 07/01/2024.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        HStack {
+            landmark.image.resizable().frame(width: 50, height: 50)
+            
+            Text(landmark.name)
+            
+            Spacer()
+            
+        }
+    }
+}
+
+#Preview() {
+    Group {
+        LandmarkRow(landmark: landmarks[0])
+        LandmarkRow(landmark: landmarks[1])    
+        LandmarkRow(landmark: landmarks[2])
+
+        
+    }
+}
+
